5f680ac1b2344fa3e7d927f8a3f83b72c3f2b429
xenma <poma@kb.dk>
2017-03-07 12:58:17 +0100

    Add a helper method to show edition names in facet

--- app/controllers/catalog_controller.rb
+++ app/controllers/catalog_controller.rb
@@ -69,7 +69,7 @@ class CatalogController < ApplicationController
     #  (useful when user clicks "more" on a large facet and wants to navigate alphabetically across a large set of results)
     # :index_range can be an array or range of prefixes that will be used to create the navigation (note: It is case sensitive when searching values)
 
-    config.add_facet_field 'cobject_edition_ssi', label: 'Edition'
+    config.add_facet_field 'cobject_edition_ssi', label: 'Edition', helper_method: :show_edition_name
     config.add_facet_field 'subject_topic_id_ssim', label: 'Kategori'
     #config.add_facet_field 'subject_topic_facet', label: 'Topic', limit: 20, index_range: 'A'..'Z'
 
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+
+  # Helper to show the name of the edition in facets
+  def show_edition_name name
+    # Find the document with the specific id
+    doc = Finder.get_doc_by_id(name)
+    doc.first['name_ssi'] unless doc.first['name_ssi'].blank?
+  end
+
 end
