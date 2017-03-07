1dd1d78904aca9bca0986f7a2fbdae519fe856d5
xenma <poma@kb.dk>
2017-03-07 15:41:29 +0100

    Add limit and collapse to facets (at least for now)

--- app/controllers/catalog_controller.rb
+++ app/controllers/catalog_controller.rb
@@ -71,8 +71,8 @@ class CatalogController < ApplicationController
     #  (useful when user clicks "more" on a large facet and wants to navigate alphabetically across a large set of results)
     # :index_range can be an array or range of prefixes that will be used to create the navigation (note: It is case sensitive when searching values)
 
-    config.add_facet_field 'cobject_edition_ssi', label: 'Edition', helper_method: :show_edition_name
-    config.add_facet_field 'subject_topic_id_ssim', label: 'Kategori', helper_method: :show_category_name
+    config.add_facet_field 'cobject_edition_ssi', label: 'Edition', helper_method: :show_edition_name, collapse: false, limit: 20
+    config.add_facet_field 'subject_topic_id_ssim', label: 'Kategori', helper_method: :show_category_name, collapse: false, limit: 10
     #config.add_facet_field 'subject_topic_facet', label: 'Topic', limit: 20, index_range: 'A'..'Z'
 
     #config.add_facet_field 'example_pivot_field', label: 'Pivot Field', :pivot => ['format', 'language_facet']
