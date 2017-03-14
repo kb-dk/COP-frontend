c31bfe123947c90f0bd5b21c32b5cd55b254fd62
xenma <poma@kb.dk>
2017-03-14 11:25:51 +0100

    Add KB's login

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170301125841) do
+ActiveRecord::Schema.define(version: 20170314101640) do
 
   create_table "bookmarks", force: :cascade do |t|
     t.integer  "user_id",       null: false
@@ -47,8 +47,11 @@ ActiveRecord::Schema.define(version: 20170301125841) do
     t.datetime "created_at",                             null: false
     t.datetime "updated_at",                             null: false
     t.boolean  "guest",                  default: false
+    t.string   "username"
+    t.string   "fullname"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["username"], name: "index_users_on_username", unique: true
   end
 
 end
