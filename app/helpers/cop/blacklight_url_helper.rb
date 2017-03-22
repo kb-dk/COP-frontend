abbdf19d32ca4a44e9a394a4c9efab5f863a3567
David Grove Jørgensen <dgj@kb.dk>
2017-03-22 12:06:23 +0100

    added locale to url

--- app/helpers/cop/blacklight_url_helper.rb
+++ app/helpers/cop/blacklight_url_helper.rb
@@ -2,7 +2,7 @@ module Cop
   module BlacklightUrlHelper
     include Blacklight::UrlHelperBehavior
     def url_for_document doc, options = {}
-      doc[:id] unless doc.nil?
+      "#{doc[:id]}/#{params[:locale]}" unless doc.nil?
     end
   end
 end
