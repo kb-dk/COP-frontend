44489d7788f645ed8fbe24ed798728b79f9b4431
David Grove Jørgensen <dgj@kb.dk>
2017-03-22 12:07:26 +0100

    added function to translate edition and subject url params into solr fq

--- app/models/search_builder.rb
+++ app/models/search_builder.rb
@@ -1,6 +1,19 @@
 # frozen_string_literal: true
 class SearchBuilder < Blacklight::SearchBuilder
   include Blacklight::Solr::SearchBuilderBehavior
+  self.default_processor_chain += [:add_edition,:add_subject]
+
+  def add_edition(solr_parameters)
+    if blacklight_params[:edition].present? && !blacklight_params[:subj_id].present?
+      solr_parameters[:fq] << "cobject_edition_ssi:\"/#{blacklight_params[:medium]}/#{blacklight_params[:collection]}/#{blacklight_params[:year]}/#{blacklight_params[:month]}/#{blacklight_params[:edition]}\""
+    end
+  end
+
+  def add_subject(solr_parameters)
+    if blacklight_params[:subj_id].present?
+      solr_parameters[:fq] << "subject_topic_id_ssim:\"/#{blacklight_params[:medium]}/#{blacklight_params[:collection]}/#{blacklight_params[:year]}/#{blacklight_params[:month]}/#{blacklight_params[:edition]}/subject#{blacklight_params[:subj_id]}\""
+    end
+  end
 
   ##
   # @example Adding a new step to the processor chain
