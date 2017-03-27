eb313f701b2b3dfe694d00438a2672375a57d927
xenma <poma@kb.dk>
2017-03-27 13:22:04 +0200

    Remove edition facet + increase the limit in kategori facet

--- app/controllers/catalog_controller.rb
+++ app/controllers/catalog_controller.rb
@@ -78,10 +78,11 @@ class CatalogController < ApplicationController
     #  (useful when user clicks "more" on a large facet and wants to navigate alphabetically across a large set of results)
     # :index_range can be an array or range of prefixes that will be used to create the navigation (note: It is case sensitive when searching values)
 
-    config.add_facet_field 'cobject_edition_ssi', label: 'Edition', helper_method: :show_edition_name, collapse: true, limit: 20, partial: 'facet_editions'
+    # config.add_facet_field 'cobject_edition_ssi', label: 'Edition', helper_method: :show_edition_name, collapse: true, limit: 20, partial: 'facet_editions'
    
-    # I put the limit at 1000 here, to get all the facets to iterate through in the _facet_category.html.erb
-   config.add_facet_field 'subject_topic_id_ssim', label: 'Kategori', helper_method: :show_category_name, collapse: false, limit: 1000 , partial: 'facet_category'
+    # I put the limit at 300000 here, to get all the facets to iterate through in the _facet_category.html.erb so we can
+    # calculate the hits. SO FAR we have ~30000 subjects in solr (search for: parent_ssi:[* TO *])
+    config.add_facet_field 'subject_topic_id_ssim', label: 'Kategori', helper_method: :show_category_name, collapse: false, limit: 300000 , partial: 'facet_category'
     config.add_facet_field 'contributor_tsim', label: 'Contributor', limit: 20
     #config.add_facet_field 'example_pivot_field', label: 'Pivot Field', :pivot => ['format', 'language_facet']
 
