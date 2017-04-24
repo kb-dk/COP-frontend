1dac828a8d285acc052412e66bcc0444762bebcb
xenma <poma@kb.dk>
2017-04-24 14:06:23 +0200

    Overwrite the 'start over' link

--- app/helpers/cop/blacklight_url_helper.rb
+++ app/helpers/cop/blacklight_url_helper.rb
@@ -4,5 +4,13 @@ module Cop
     def url_for_document doc, options = {}
       "#{doc[:id]}/#{params[:locale]}/" unless doc.nil?
     end
+
+    ##
+    # Get the path to the search action with any parameters (e.g. view type)
+    # that should be persisted across search sessions.
+    def start_over_path query_params = params
+      params['locale'].blank? ? lang = "da" : lang = params['locale']
+      "/editions/any/2009/jul/editions/"+lang+"/"
+    end
   end
 end
