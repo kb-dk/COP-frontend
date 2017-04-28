cff70f78adbb60103b5933fcd5ace273178143cd
xenma <poma@kb.dk>
2017-04-28 13:57:29 +0200

    Add tidligst and senest

--- app/controllers/catalog_controller.rb
+++ app/controllers/catalog_controller.rb
@@ -157,7 +157,9 @@ class CatalogController < ApplicationController
     # whether the sort is ascending or descending (it must be asc or desc
     # except in the relevancy case).
     config.add_sort_field 'score desc', label: '' #search after relevance
-    config.add_sort_field 'cobject_title_ssi asc, score desc', label: 'title'
+    config.add_sort_field 'cobject_title_ssi asc, score desc', label: 'Titel'
+    config.add_sort_field 'cobject_not_before_dtsi asc', label: 'Tidligst'
+    config.add_sort_field 'cobject_not_after_dtsi desc', label: 'Senest'
 
 
     # If there are more than this many search results, no spelling ("did you
