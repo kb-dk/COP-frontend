2b16c55ea8eac1dad532b1d4ec9fc702cdbaa1da
Lauréanne <laap@kb.dk>
2017-06-14 11:24:00 +0200

    hack to ensure smaller thumbnail images

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -51,8 +51,12 @@ module ApplicationHelper
 
   def show_scaled_image(doc, opts)
     uri = doc['thumbnail_url_ssm'].first
-    if uri[/ull\/ful/]
+    if uri[/full\/full/]
       uri = uri.gsub(/full\/full/,'full/!225,')
+    end
+    #hack: needs to be removed when we are sure that all thumbnails have the right size 
+    if uri[/full\/!400/]
+      uri = uri.gsub(/full\/!400/,'full/!225')
     end
     img_tag = image_tag(URI(uri))
     return img_tag
