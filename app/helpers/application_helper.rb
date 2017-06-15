d4a3df0a64a7f51bfbca05426c9fc1bea7e96a02
bimo <zahramousavidk@gmail.com>
2017-06-15 13:32:39 +0200

    Separate the helpers for the tree

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,7 @@
 module ApplicationHelper
 
+  include Tree::TreeNodesHelper
+
   # Helper to show the name of the edition in facets
   def show_edition_name id
     # Find the document with the specific id
@@ -41,33 +43,6 @@ module ApplicationHelper
     return lang
   end
 
-  # Helper to get the breadcrumb for a category
-  def get_breadcrumb_path cat_id
-    # Find the document with the specific id
-    doc = Finder.get_doc_by_id(cat_id)
-    # add its own id in breadcrumb
-    doc['bread_crumb_ssim'].nil? ? breadcrumb = [] : breadcrumb = doc['bread_crumb_ssim']
-    # Get the array and revert the order
-    return breadcrumb.reverse << cat_id
-  end
-
-  # Helper to get the siblings of a category
-  def get_siblings cat_id
-    # Find the document with the specific id
-    doc = Finder.get_doc_by_id(cat_id)
-    # Get the parent id
-    parent = doc['parent_ssi']
-    # Find the subcategories of this parent
-    siblings = Finder.get_subcats_by_id(parent)
-    return siblings
-  end
-
-  # Helper to get the
-  def get_children cat_id
-    # Find the subcategories of a specific id
-    children = Finder.get_subcats_by_id(cat_id)
-    return children
-  end
 
   def show_scaled_image(doc, opts)
     uri = doc['thumbnail_url_ssm'].first
