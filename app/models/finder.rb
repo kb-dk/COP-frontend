d77de8f13db04533ffcc47a00f25275fa703ccf1
David Grove Jørgensen <dgj@kb.dk>
2018-05-15 14:51:06 +0200

    better caching

--- app/models/finder.rb
+++ app/models/finder.rb
@@ -7,17 +7,21 @@ class Finder
 
   # Function to return the solr document by id
   def self.get_doc_by_id(id)
-    query = "id:#{id}"
-    response = get_solr.get 'select', :params => {:q => query}
-    # Return the first document
-    return response['response']['docs'].first
+    Rails.cache.fetch("document/#{id}", expires_in: 3.hours) do
+      query = "id:#{id}"
+      response = get_solr.get 'select', :params => {:q => query}
+      # Return the first document
+      response['response']['docs'].first
+    end
   end
 
   # Function to return the solr documents that are subcategories, searching by id
   def self.get_subcats_by_id id
-    query = "parent_ssi:#{id}" # Set the query
-    response = get_solr.get 'select', :params => {:q => query, :rows => 1000} # Return all the subcategories (by default the rows: 10)
-    return response['response']['docs'].delete_if { |x| x["id"]=="/images/luftfo/2011/maj/luftfoto/subject203" } # Return all the documents except luftfoto
+    Rails.cache.fetch("subcats/#{id}", expires_in: 3.hours) do
+      query = "parent_ssi:#{id}" # Set the query
+      response = get_solr.get 'select', :params => {:q => query, :rows => 1000} # Return all the subcategories (by default the rows: 10)
+      response['response']['docs'].delete_if { |x| x["id"]=="/images/luftfo/2011/maj/luftfoto/subject203" } # Return all the documents except luftfoto
+    end
   end
 
 end
